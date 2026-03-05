2e0c3c70f0f96bc2acc0c0e7e004929fca48e4d1
EloyArS <eloy002.ea@gmail.com>
2026-03-05 12:37:10 +0100

    cambios en código index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
-// Importas las rutas con tu nueva constante
+
 const TaskRoutes = require('./routes/routes'); 
 
 app.use(express.json());
-app.use('/tasks', TaskRoutes); // Usas la constante aquí
+app.use('/tasks', TaskRoutes);
 
-app.listen(3000, () => console.log("🚀 Servidor listo"));
+app.get('/', (req, res) => {
+    res.send('<h1>Bienvenido a mi Task API</h1><p>Ve a <a href="/tasks">/tasks</a> para ver los datos.</p>');
+});
+
+app.listen(3000, () => console.log("Servidor operativo"));
